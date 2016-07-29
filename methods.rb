55e29d888bd6be0173db45a939ae31656bcce630
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-07-29 18:28:45 +0300

    add method get_secret_word

--- methods.rb
+++ methods.rb
@@ -1,2 +1,34 @@
-# Test
-puts "Hello, world!"
+def get_secret_word
+	# возвращает массив букв из того слова
+	# которое пользователь загадывал в консоли
+	secret_word = ARGV[0]
+
+	if (secret_word == nil || secret_word == "")
+		abort "Вы не ввели слово для игры"
+	end
+	
+	return secret_word.split("")
+end
+
+
+
+
+
+end
+
+def get_user_input
+
+
+
+end
+
+def check_result
+
+end
+
+
+def print_status
+
+
+
+end
