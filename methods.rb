b64fd2b4a1f4bb0a41b3bc8a073351545f922a4a
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-07-29 23:08:51 +0300

    add get_user_input

--- methods.rb
+++ methods.rb
@@ -6,21 +6,20 @@ def get_secret_word
 	if (secret_word == nil || secret_word == "")
 		abort "Вы не ввели слово для игры"
 	end
-	
+
 	return secret_word.split("")
 end
 
-
-
-
-
-end
-
 def get_user_input
+	letter = ""
 
+	while letter == "" do
+		letter = STDIN.gets.encode("UTF-8").downcase.chomp
+	end
 
-
+	return letter
 end
+
 
 def check_result
 
