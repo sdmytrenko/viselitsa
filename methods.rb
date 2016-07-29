1a97063d1d4fe28806ca63e171748edb167a32ae
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-07-29 23:18:19 +0300

    add check_result

--- methods.rb
+++ methods.rb
@@ -21,10 +21,34 @@ def get_user_input
 end
 
 
-def check_result
+def check_result(user_input, secret_word, good_letters, bad_letters)
+	if (good_letters.include?(user_input) ||
+		bad_letters.include?(user_input))
+		#Если эта буква есть в good_letters или bad_letters,
+		# то она дальше не пойдет по циклу
 
+		puts "Вы уже называли эту букву"
+		return 0
+	end
+
+	if secret_word.include? user_input
+		good_letters << user_input
+		#Только если буква входит в массив, мы наполняем good_letters
+
+		if secret_word.uniq.size == good_letters.size
+			#Условие когда отгадано все слово	
+			#uniq - извлечение только уникальных букв
+			# Если длинна уникальных букв == к-ву хороших букв
+			
+			return 1
+		else
+			return 0
+		end
+	else
+		bad_letters << user_input
+		return -1
+	end
 end
-
 
 def print_status
 
